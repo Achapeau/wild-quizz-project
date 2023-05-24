129eb496f5c5078fb22150e50502e1ddc68b245a
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-05-24 18:47:31 +0200

    creating burgerMenu.js and adding line in ressources.html

--- JS/questions.js
+++ JS/questions.js
@@ -1,4 +1,4 @@
-let questions = [
+let questionsDarwin = [
   {
     numb: 1,
     question:
@@ -49,7 +49,7 @@ let questions = [
       "Son chien fidèle lui a rapporté le bâton de dynamite",
       "Il n'a pas lancé le bâton assez loin",
       "La glace n'était pas assez solide et tout le lac s'est fissuré après l'explosion",
-      "Il y a eu une avalanche",
+      "L'explosion a déclenché une avalanche",
     ],
   },
 
@@ -134,5 +134,270 @@ let questions = [
       "Il a voulu organiser une messe dans le désert. Il est mort de déshydratation",
       "Le record de celui qui mange le plus de cornichons en moins de 30 minutes",
     ],
+  },
+];
+
+let questionsCinema = [
+  {
+    numb: 1,
+    question: "Qui joue le fameux soldat Ryan dans le film éponyme ?",
+    answer: "Matt Damon",
+    options: ["Vin Diesel", "Tom Hanks", "Jude Law", "Matt Damon"],
+  },
+
+  {
+    numb: 2,
+    question: "Quelle actrice joue le rôle d’Hermione Granger ?",
+    answer: "Emma Watson",
+    options: ["Evanna Lynch", "Bonnie Wright", "Emma Watson", "Emma Thompson"],
+  },
+
+  {
+    numb: 3,
+    question: "A quelle maison appartient Drago Malefoy ?",
+    answer: "Serpentard",
+    options: ["Serdaigle", "Gryffondor", "Poufsouffle", "Serpendard"],
+  },
+
+  {
+    numb: 4,
+    question: "Quel acteur incarne Scarface dans le film de Brian de Palma ?",
+    answer: "Al Pacino",
+    options: ["Robert de Niro", "Martin Sheen", "Kevin Bacon", "Al Pacino"],
+  },
+
+  {
+    numb: 5,
+    question: "Dans la saga Star Wars, qui est le père de Luke Skywalker ?",
+    answer: "Dark Vador",
+    options: ["Dark Sidious", "Obi-Wan Kenobi", "Dark Vador", "Chewbacca"],
+  },
+
+  {
+    numb: 6,
+    question:
+      " Dans combien de films, Johnny Depp incarne-t-il Jack Sparrow dans la saga Pirates des Caraïbes ?",
+    answer: "5 films",
+    options: ["3 films", "4 films", "5 films", "6 films"],
+  },
+
+  {
+    numb: 7,
+    question:
+      "Quel est le nom du petit monstre gentil, héros du film Les Gremlins ?",
+    answer: "Guizmo",
+    options: ["Guizmo", "Willow", "Tobby", "Dobby"],
+  },
+
+  {
+    numb: 8,
+    question:
+      "Quelle actrice incarne Mary Jane Watson dans la trilogie Spider-Man ?",
+    answer: "Kirsten Dunst",
+    options: ["Cate Blanchett", "Emma Stone", "Zendaya", "Kirsten Dunst"],
+  },
+
+  {
+    numb: 9,
+    question: "Qui a réalisé le film 'Forrest Gump' ?",
+    answer: "Robert Zemeckis",
+    options: [
+      "Robert Zemeckis",
+      "Tom Hanks",
+      "Les frères Cohen",
+      "James Cameron",
+    ],
+  },
+
+  {
+    numb: 10,
+    question:
+      "Quel acteur joue le rôle du gardien-chef Edgecomb dans le film 'La ligne verte' ?",
+    answer: "Tom Hanks",
+    options: ["Sam Rockwell", "Tom Hanks", "Michael Clarke", "David Morse"],
+  },
+];
+
+let questionsCultureG = [
+  {
+    numb: 1,
+    question: "Quel est le dessert préféré d’Homer Simpson ?",
+    answer: "Les donuts",
+    options: [
+      "Les éclairs au chocolat",
+      "Les donuts",
+      "Les brownies",
+      "Les gaufres à la vanille",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "Quel groupe a interprété la chanson « I Gotta Feeling » ?",
+    answer: "Black Eyed Peas",
+    options: [
+      "Eminen",
+      "Red Hot Chili Peppers",
+      "Black Eyed Peas",
+      "Audioslave",
+    ],
+  },
+
+  {
+    numb: 3,
+    question:
+      "Quel numéro d'urgence devez-vous composer pour joindre les pompiers ?",
+    answer: "18",
+    options: ["15", "16", "18", "112"],
+  },
+
+  {
+    numb: 4,
+    question: "À partir de quel âge peut-on obtenir le permis B ?",
+    answer: "18 ans",
+    options: ["15 ans", "16 ans", "18 ans", "21 ans"],
+  },
+
+  {
+    numb: 5,
+    question:
+      "Quelle est la vitesse maximale de circulation sur une autoroute ?",
+    answer: "130 km/h",
+    options: ["90 km/h", "110 km/h", "130 km/h", "140 km/h"],
+  },
+
+  {
+    numb: 6,
+    question: "De quel fruit l'Eureka est-il une variété ?",
+    answer: "Le citron",
+    options: ["Le cassis", "Le citron", "La pastèque", "Le raisin"],
+  },
+
+  {
+    numb: 7,
+    question: "Que fête-t-on le 1er mai ?",
+    answer: "La fête du travail",
+    options: [
+      "La fête du printemps",
+      "La fête des mères",
+      "La fête du travail",
+      "La fête du Beaujolais",
+    ],
+  },
+
+  {
+    numb: 8,
+    question: " Quel arbre produit la noix de pécan ?",
+    answer: "Le pacanier",
+    options: ["Le macadamia", "Le noisetier", "Le pacanier", "Le pécunier"],
+  },
+
+  {
+    numb: 9,
+    question: "Qui est l’inséparable compagnon de Titi ?",
+    answer: "Grosminet",
+    options: ["Toto", "Grosminet", "Tata", "Jerry"],
+  },
+
+  {
+    numb: 10,
+    question:
+      "Quelle est la seule région dépassant les 10 millions d’habitants ?",
+    answer: "Île-de-France",
+    options: [
+      "Auvergne-Rhône-Alpes",
+      "Aquitaine-Limousin-Poitou-Charentes",
+      "Île-de-France",
+      "Provence-Alpes-Côte d'Azur",
+    ],
+  },
+];
+
+let questionsSport = [
+  {
+    numb: 1,
+    question: "Quel est le nom du sélectionneur des Bleus en 2016 ?",
+    answer: "Laurent Blanc",
+    options: [
+      "Michel Platini",
+      "Didier Deschamps",
+      "Laurent Blanc",
+      "Claude Puel",
+    ],
+  },
+
+  {
+    numb: 2,
+    question: "Quel footballeur a été élu Ballon d’or 2015 ?",
+    answer: "Lionel Messi",
+    options: ["Cristiano Ronaldo", "Neymar", "Lionel Messi", "Thomas Müller"],
+  },
+
+  {
+    numb: 3,
+    question:
+      "Dans quel club de NBA évolue le basketteur Kobe Bryant depuis 1996 ?",
+    answer: "Los Angeles Lakers",
+    options: [
+      "San Antonio Spurs",
+      "Orlando Magic",
+      "Utah Jazz",
+      "Los Angeles Lakers",
+    ],
+  },
+
+  {
+    numb: 4,
+    question: "Dans quelle équipe de NBA joue Joachim Noah ?",
+    answer: "Chicago Bullss",
+    options: [
+      "Los Angeles Lakers",
+      "Chicago Bulls",
+      "San Antonio Spurs",
+      "New York Knicks",
+    ],
+  },
+
+  {
+    numb: 5,
+    question:
+      "Quelle est la nationalité d’Usein Bolt, recordman du monde sur 100 mètres ?",
+    answer: "Jamaïcaine",
+    options: ["Nigériane", "Américaine", "Jamaïcaine", "Trinidadienne"],
+  },
+
+  {
+    numb: 6,
+    question: " De quelle forme est un ballon de rugby ?",
+    answer: "Ovale",
+    options: ["Ovale", "Rond", "Triangulaire", "Carré"],
+  },
+
+  {
+    numb: 7,
+    question: "Sur quelle surface joue-t-on au billard ?",
+    answer: "Une table",
+    options: ["Une arène", "Une table", "Une piste", "Une patinoire"],
+  },
+
+  {
+    numb: 8,
+    question: "Quel est le sport de prédilection de Raphael Nadal ?",
+    answer: "Tennis",
+    options: ["Basket-ball", "Hand-ball", "Cyclisme", "Tennis"],
+  },
+
+  {
+    numb: 9,
+    question: "Quel sport compte le plus grand nombre de licenciés en France ?",
+    answer: "Le football",
+    options: ["Le football", "La natation", "Le badminton", "Le tennis"],
+  },
+
+  {
+    numb: 10,
+    question: "Où se courent les 24 Heures du Mans ?",
+    answer: "Au Mans",
+    options: ["A Daytona", "A Imola", "Au Mans", "A Dubaï"],
   },
 ];
