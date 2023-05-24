3ee1bede95b9776510585795a77dba9936d06aca
ANAIS CAVAILLE <anais.cavaille93@gmail.com>
2023-05-24 15:45:46 +0200

    creating questions.js and adding 1st theme

---
+++ JS/questions.js
@@ -0,0 +1,138 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Comment Garry Holy, un homme de 39 ans, est-il tombé du 24ème étage d'un immeuble ?",
+    answer:
+      "Il a donné un coup d'épaule dans la vitre du 24ème étage pour prouver sa solidité à son entourage",
+    options: [
+      "Il s'est pris pour Superman",
+      "Il a donné un coup d'épaule dans la vitre du 24ème étage pour prouver sa solidité à son entourage",
+      "Il avait pris le pari qu'il ne mourrait pas en sautant de cette hauteur",
+      "Il a cru qu'il s'était transformé en oiseau",
+    ],
+  },
+
+  {
+    numb: 2,
+    question:
+      "Comment un homme de 41 ans est-il mort à Détroit en essayant de ramasser ses clés de voiture dans la rue ?",
+    answer:
+      "Il est mort noyé en tentant de récupérer ses clefs dans une bouche d'égout, sa tête étant resté coincée entre les grilles de celle-ci ",
+    options: [
+      "Son porte-clefs était équipé d'explosifs qui ont explosé lorsqu'il l'a ramassé",
+      "Il est mort noyé en tentant de récupérer ses clefs dans une bouche d'égout, sa tête étant resté coincée entre les grilles de celle-ci ",
+      "Un homme lui a volé ses clefs lorsque celles-ci sont tombées. Notre malheureux a poursuivi le malfaiteur pour récupérer son bien, mais ce dernier l'a poignardé",
+      "Il a trébuché sur un caillou",
+    ],
+  },
+
+  {
+    numb: 3,
+    question:
+      " Après la mort d'un homme lors de son jogging quotidien, sa femme a déclaré qu'il faisait le vide dans sa tête lorsqu'il courait, et en effet ...",
+    answer: "Il a chuté dans un ravin de 60 mètres",
+    options: [
+      "Il n'a pas vu arriver le poteau en face de lui durant sa course",
+      "Il est entré en collision avec un TGV",
+      "Il a chuté dans un ravin de 60 mètres",
+      "Il s'est fait courser par un ours plus rapide que lui",
+    ],
+  },
+
+  {
+    numb: 4,
+    question:
+      " Un homme voulant pêcher sur un lac gelé en Alaska a eu la mauvaise idée de lancer un bâton de dynamite sur la glace pour la briser. Que s'est-il passé après le jet du bâton",
+    answer: " Son chien fidèle lui a rapporté le bâton de dynamite",
+    options: [
+      "Son chien fidèle lui a rapporté le bâton de dynamite",
+      "Il n'a pas lancé le bâton assez loin",
+      "La glace n'était pas assez solide et tout le lac s'est fissuré après l'explosion",
+      "Il y a eu une avalanche",
+    ],
+  },
+
+  {
+    numb: 5,
+    question:
+      "Faites attention en faisant le ménage... qu'à utilisée une femme pour nettoyer les toiles d'araignées dans sa cave au lieu d'un traditionnel balai ?",
+    answer: "Une torche enflammée qui a mis feu à sa maison",
+    options: [
+      "Un tuyau d'arrosage qu'elle n'a pas pu arrêter : elle est morte noyée",
+      "Une torche enflammée qui a mis feu à sa maison",
+      "Un souffleur de feuilles utilisé normalement en extérieur et qui a exploser durant son ménage",
+      "Elle a acheté un python qui a préférer la manger elle que les araignées",
+    ],
+  },
+
+  {
+    numb: 6,
+    question:
+      "Où s'est installé un jeune homme après une soirée bien arrosée lui causant la mort le lendemain matin au Luxembourg ?",
+    answer:
+      "Dans une benne à ordure, le lendemain il a terminé dans le broyeur du camion des éboueurs",
+    options: [
+      "Sur un rail de train, le lendemain logiquement un train est passé",
+      "Sur un container dans un bateau, le lendemain le container est tombé du bateau entraînant ainsi le malheureux voyageur",
+      "Dans une benne à ordure, le lendemain il a terminé dans le broyeur du camion des éboueurs",
+      "Chez son ex",
+    ],
+  },
+
+  {
+    numb: 7,
+    question:
+      "Surement la mort la plus ridicule pour un personnage historique, cela se passe en 1498, comment est mort le roi de France Charles VIII ?",
+    answer:
+      "Il meurt après avoir heurté un linteau de porte au château d'Amboise",
+    options: [
+      "Il meurt après avoir heurté un linteau de porte au château d'Amboise",
+      "Il chute de la fenêtre de sa chambre en voulant remettre ses rideaux en place",
+      "Il s'étouffe avec un raisin lors d'un repas destiné à fêter son anniversaire",
+      "Il est tombé de son cheval",
+    ],
+  },
+
+  {
+    numb: 8,
+    question:
+      "Comment est mort l'astronome danois Tycho Brahe créant ainsi l'expression 'Je ne veux pas mourir comme Tycho Brahe' au Danemark ?",
+    answer:
+      "Il serait mort d'un calcul après s'être retenu trop longtemps d'uriner durant un long trajet : l'expression prétextant donc une envie pressante",
+    options: [
+      "Il serait mort en buvant trop d'eau : l'expression étant donc utilisé lors d'apéritifs pour faire comprendre qu'un convive ne prendra pas d'eau",
+      "Il serait mort d'un calcul après s'être retenu trop longtemps d'uriner durant un long trajet : l'expression prétextant donc une envie pressante",
+      "Il serait mort en mangeant trop lors d'un festin : l'expression signifiant donc 'j'arrête de manger' lors d'un repas",
+      "Il serait mort en jouant à la pétanque : l'expression signifiant donc 'Tu tires ou tu pointes?'",
+    ],
+  },
+
+  {
+    numb: 9,
+    question:
+      "Comment Robert Puelo est-il mort après avoir volé un hot-dog sur un marché ?",
+    answer:
+      "Il s'est étouffé avec la saucisse et le SAMU n'a pas pu le ranimer",
+    options: [
+      "Il s'est étouffé avec la saucisse et le SAMU n'a pas pu le ranimer",
+      "Il s'est fait tirer dessus par le vendeur de hot-dogs",
+      "Il a fait tomber son butin et a glissé dessus. La chute lui a été fatale",
+      "Il s'est fait tuer par un SDF qui avait faim",
+    ],
+  },
+
+  {
+    numb: 10,
+    question:
+      "Quel record du monde un révérend brésilien a-t-il voulu battre, mais qui lui a finalement coûté la vie ?",
+    answer:
+      "Le record du plus long vol accroché à des ballons de baudruche. Il fut retrouvé mort trois mois après son décollage sur la côte brésilienne.",
+    options: [
+      "Le record du nombre d'heures sans se nourrir",
+      "Le record du plus long vol accroché à des ballons de baudruche. Il fut retrouvé mort trois mois après son décollage sur la côte brésilienne.",
+      "Il a voulu organiser une messe dans le désert. Il est mort de déshydratation",
+      "Le record de celui qui mange le plus de cornichons en moins de 30 minutes",
+    ],
+  },
+];
