a53254254b404088d315fbec168920528755046e
CHAPEAU Adrien <ad.chap.13.17@gmail.com>
2023-05-25 14:15:00 +0200

    first part of js results

--- JS/results.js
+++ JS/results.js
@@ -1,18 +1,18 @@
-let stockCorrectAnswer = 0;
-let stockNumberOfQuestions = 0;
+let stockCorrectAnswer = 5;
+let stockNumberOfQuestions = 10;
 
 //ici, la variable right est stockée dans le localstorage, avec la valeur associée
 
-const correct = document.querySelector(".answer1");
+/* const correct = document.querySelector(".answer1");
 
 /* ici, on cible la classe answer1 comme une bonne réponse.
 Si on créé un ID qui n'a pas un emplacement fixe lors de la génération de réponse du quizz,
 et qu'on le cible, il n'y a plus de soucis de réponse fixe : à modifier*/
-correct.addEventListener("click", () => {
-  stockCorrectAnswer++;
-  localStorage.setItem("right", stockCorrectAnswer);
-  alert(localStorage.right);
-}); /*function comments */
+/*correct.addEventListener("click", () => {
+  stockCorrectAnswer++; */
+localStorage.setItem("right", stockCorrectAnswer);
+
+/*});  function comments */
 let valueResults =
   localStorage.getItem("right"); /* récupère la valeur de 'right' 
         depuis le local storage pour la mettre dans 'valueResults'*/
@@ -26,6 +26,7 @@ const scoreQuizz =
 scoreQuizz.textContent = `${score} / 20`;
 
 /* affichage du commentaire */
+
 const comments =
   document.querySelector("#comments"); /*cible le contenu de l'ID "comments" */
 
@@ -42,4 +43,66 @@ if (valueResults === undefined) {
   /*si vous voulez insérer des confettis, ça sera dans cette partie*/
   comments.textContent =
     "A tout le temps de bons résultats, est soupçonné de cheater";
+}
+valueResults = 10;
+/* Génération d'un tableau des scores */
+localStorage.setItem("score5", localStorage.score4);
+localStorage.setItem("score4", localStorage.score3);
+localStorage.setItem("score3", localStorage.score2);
+localStorage.setItem("score2", localStorage.score1);
+localStorage.setItem("score1", valueResults);
+
+/*permet de faire remonter les scores au fur et à mesure */
+
+const resultsMain = document.querySelector(".results-main");
+
+const scoreArray = [
+  {
+    id: "le plus récent",
+    resultat: localStorage.score1,
+  },
+  {
+    id: "2ème",
+    resultat: localStorage.score2,
+  },
+  {
+    id: "3ème",
+    resultat: localStorage.score3,
+  },
+  {
+    id: "4ème",
+    resultat: localStorage.score4,
+  },
+  {
+    id: "5ème",
+    resultat: localStorage.score5,
+  },
+];
+const visualArray = document.createElement("div");
+visualArray.classList.add("visual-array");
+resultsMain.appendChild(visualArray);
+
+const scoreTitle = document.createElement("h2");
+scoreTitle.classList.add("score-title");
+scoreTitle.textContent = "Tableau des scores précédents";
+visualArray.appendChild(scoreTitle);
+
+/* Création de la fonction de génération */
+
+function createArray(position, score) {
+  /* Création du tableau position */
+
+  const positionClassment = document.createElement("div");
+  positionClassment.classList.add("position-classment");
+  positionClassment.textContent = `${position}`;
+  visualArray.appendChild(positionClassment);
+
+  const scoreSave = document.createElement("div");
+  scoreSave.classList.add("score-save");
+  scoreSave.textContent = `${score}`;
+  visualArray.appendChild(scoreSave);
+}
+
+for (let i = 0; i < scoreArray.length; i++) {
+  createArray(scoreArray[i].id, scoreArray[i].resultat);
 }
